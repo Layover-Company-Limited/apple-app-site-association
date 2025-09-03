203d489bf4928268655f69b963b13a481f0450ca
jkv21 <84196753+jackyleung2436@users.noreply.github.com>
2025-09-03 15:22:21 +0800

    redirect method

--- middleware.js
+++ middleware.js
@@ -4,10 +4,8 @@ export function middleware(request) {
   const { pathname } = request.nextUrl
 
   // If path starts with /.well-known/, 
-  if (pathname.startsWith('/.well-known/')) {
-    const url = new URL(request.url)
-    url.hostname = 'https://apple-app-site-association-nine.vercel.app' // target host
-    return NextResponse.rewrite(url) // proxy
+  if (pathname.startsWith('/zh-HK/.well-known/')|| pathname.startsWith('/.well-known/')) {
+    return NextResponse.next()
   }
 
   // Otherwise, send traffic to Odoo
