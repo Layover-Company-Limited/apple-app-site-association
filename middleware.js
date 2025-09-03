c2bc2f27e9e7e902e3cb1761a7f1b4c800b42aab
jkv21 <84196753+jackyleung2436@users.noreply.github.com>
2025-09-03 17:17:12 +0800

    domain name fix

--- middleware.js
+++ middleware.js
@@ -4,10 +4,10 @@ export function middleware(request) {
   const { pathname } = request.nextUrl
 
   // If path starts with /.well-known/, 
-  if (pathname.startsWith('/zh-HK/.well-known/')|| pathname.startsWith('/.well-known/')) {
+  if (pathname.startsWith('/zh_HK/.well-known/')|| pathname.startsWith('/.well-known/')) {
     return NextResponse.next()
   }
 
   // Otherwise, send traffic to Odoo
   return NextResponse.rewrite('https://layover-ai.odoo.com' + pathname)
-}
+} 
