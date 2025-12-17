8c7926ff442fb593d8e6ad22e1165fb1bc0145ee
jkv21 <84196753+jackyleung2436@users.noreply.github.com>
2025-12-17 09:58:56 +0800

    app-ads.txt

--- middleware.js
+++ middleware.js
@@ -9,7 +9,7 @@ export function middleware(request) {
   }
 
     // If path starts with /app-ads/, 
-  if (pathname.startsWith('/zh_HK/app-ads/')|| pathname.startsWith('/ads/')) {
+  if (pathname.startsWith('/zh_HK/app-ads.txt/')|| pathname.startsWith('/app-ads.txt/')) {
     return NextResponse.next()
   }
 
